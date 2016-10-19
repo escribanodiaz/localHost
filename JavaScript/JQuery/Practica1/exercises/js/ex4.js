b1ee1e4e29ac530597bb130fde6a118ffeb0c208
escribanodiaz <aescribanodiaz@gmail.com>
2016-10-19 12:58:14 +0200

    JQuery ejercicio 5 nueva pagina

--- JavaScript/JQuery/Practica1/exercises/js/ex4.js
+++ JavaScript/JQuery/Practica1/exercises/js/ex4.js
@@ -2,6 +2,7 @@ $("document").ready(function() {
   alert("hola");
 
 console.log($("a").length);
-console.log($("a:nth-last-child(1)").html());
-
+console.log($("a:nth-last-child(2)").html());
+console.log($("a[href='http://prueba']").length);
+console.log($("p:nth-child(3) a").length);
 });
